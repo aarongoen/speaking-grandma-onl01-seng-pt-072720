479c8b5f9397502783df893169f73ace28dc1564
Aaron Goen <aarongoen2.0@gmail.com>
2020-07-30 12:37:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
-     "HUH?! SPEAK UP, SONNY!"
+     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-     "NO, NOT SINCE 1938!"
+     puts "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
-     "I LOVE YOU TOO PUMPKIN!"
+     puts "I LOVE YOU TOO PUMPKIN!"
 end
